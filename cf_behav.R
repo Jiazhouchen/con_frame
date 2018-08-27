cc40668f2355dca63e98997e1cd4bc2f1c2c450c
Jiazhou Chen <rgagechen@gmail.com>
2018-08-27 13:04:24 -0400

    init

--- cf_behav.R
+++ cf_behav.R
@@ -1,7 +1,7 @@
 #Behavioral data processing function;
 
 
-proc_behav_cf<-function(boxdir=NULL,datafolder=NULL,fmriproc=T,behavproc=F,outputdir=NULL) {
+proc_behav_cf<-function(boxdir=NULL,datafolder=NULL,fmriproc=F,behav.list=F,behav.df=F,outputdir=NULL) {
   
   if (grepl(" ",boxdir)) {
     sub("Box Sync","/'Box Sync/'",boxdir)->boxdir
@@ -36,16 +36,80 @@ proc_behav_cf<-function(boxdir=NULL,datafolder=NULL,fmriproc=T,behavproc=F,outpu
   cfx<-lapply(cf_list_x,function(x) {do.call(rbind,x[which(sapply(x,is.data.frame))])})
   for (kz in names(cfx)) {
     cfx[[kz]]$ID<-kz
+
   }
   cfxz<-do.call(rbind,cfx)
-  cfxz$DRUG<-NA
-  cfxz$DRUG[grep("_Plac",cfxz$ID)]<-"Plac"
-  cfxz$DRUG[grep("_Nalt",cfxz$ID)]<-"Nalt"
-  
-  if (fmriproc) {return(as.list(tempenvir))} else if (behavproc) {return(cfxz)
-    if (!is.null(outputdir)) {
+
+  if (fmriproc) {return(as.list(tempenvir))} else if (behav.df) {return(cfxz)} else if (behav.list) {return(cfx)}
+    
+  if (!is.null(outputdir)) {
       write.csv(cfxz,file.path("compiled_conframe_behavdata.csv"))}
-    }
-  
 }
+
+proc_singlesub_cf<-function(CF) {
+  CF_a<-lapply(CF, function(x) {
+    x$DRUG<-NA
+    x$DRUG[grep("_Plac",x$ID)]<-"Plac"
+    x$DRUG[grep("_Nalt",x$ID)]<-"Nalt"
+    
+    #Rename variables
+    x$Rating<-NA
+    x$Rating[x$FaceResponseText=='Positive'] <-1
+    x$Rating[x$FaceResponseText=='Negative'] <-0
+    
+    x$ContextNum<-NA
+    x$ContextNum[x$Context=='Pleasant'] <-1
+    x$ContextNum[x$Context=='Unpleasant'] <-0
+    
+    x$EmotionNum<-NA
+    x$EmotionNum[x$Emotion=='Happy'] <-2
+    x$EmotionNum[x$Emotion=='Neutral'] <-1
+    x$EmotionNum[x$Emotion=='Fearful'] <-0
+    
+    x$Rating<-as.factor(x$Rating)
+    x$ContextNum<-as.factor(x$ContextNum)
+    x$EmotionNum<-as.factor(x$EmotionNum)
+    
+  })
+}
+
+#Do if outlier stuff
+hist(bdf$rts1,1000)
+hist(bdf$rts2,1000)
+
+# missed trials -- 2-3%
+mean(bdf$rts1==0)
+mean(bdf$rts2==0)
+
+bdf$missed <- bdf$rts1==0 | bdf$rts2==0
+bdf$outlier <- bdf$rts1<.2 | bdf$rts2<.2 | bdf$rts1 > 4 | bdf$rts2 > 4
+bdf$outlier_stay <- bdf$rts1<.2 | bdf$rts1 > 4 
+
+
+#VIF function
+vif.lme <- function (fit) {
+  ## adapted from rms::vif
+  v <- vcov(fit)
+  nam <- names(fixef(fit))
+  ## exclude intercepts
+  ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+  if (ns > 0) {
+    v <- v[-(1:ns), -(1:ns), drop = FALSE]
+    nam <- nam[-(1:ns)] }
+  d <- diag(v)^0.5
+  v <- diag(solve(v/(d %o% d)))
+  names(v) <- nam
+  v }
+
+CF<-proc_singlesub_cf(proc_behav_cf(boxdir = "/Volumes/bek/Box Sync",behav.list = T))
+
+
+save(CF,file = "cf_behav_data.rdata")
+#Separate single sub proc as a different function for easy editing:
+
+
+
+
+
+
 
