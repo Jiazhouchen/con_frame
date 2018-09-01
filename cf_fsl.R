a5232c108cfbdc3e32936a93a299bcdd4506d986
Jiazhou Chen <rgagechen@gmail.com>
2018-09-01 14:28:11 -0400

    new layout of running models

--- cf_fsl.R
+++ cf_fsl.R
@@ -8,65 +8,52 @@ if("dependlab" %in% installed.packages()){"GREAT, DEPENDLAB PACK IS INSTALLED"}e
 devtools::source_url("https://raw.githubusercontent.com/Jiazhouchen/pecina/master/pecina_R_utility_function.R")
 devtools::source_url("https://raw.githubusercontent.com/DecisionNeurosciencePsychopathology/fMRI_R/master/dnpl_utility.R")
 devtools::source_url("https://raw.githubusercontent.com/DecisionNeurosciencePsychopathology/fMRI_R/master/fslpipe.R")
-
-source("cf_behav.R")
+devtools::source_url("https://raw.githubusercontent.com/Jiazhouchen/con_frame/master/cf_behav.R")
 #Setting up FSL global enviroment variables in case we are using RStudio 
 fsl_2_sys_env()
 
 #Load in behavioral data:
 #boxdir<-findbox()
 boxdir <- "/Volumes/bek/Box Sync"
-
-#Setting some global options (Putting moving variables here so the function down there could just grab them)
-argu<-as.environment(list(
-  #Number of processes to allow for paralle processing
-  nprocess=4,
-  #If at any point you wish to stop the function, input step number here: ; if NULL then will be ignored.
-  onlyrun=NULL,
-  #Where is the cfg config file:
-  cfgpath="/Volumes/bek/autopreprocessing_pipeline/Neurofeedback/con_framing.cfg",
-  #Where to put/are the regressors 
-  regpath="/Volumes/bek/neurofeedback/sonrisa2/con_framing/regs/R_fsl_reg",
-  #Where is the grid to make signal?
-  gridpath="grid_sc.csv",
-  #What pre-proc data to grab:
-  func.nii.name="nfswudktm*[0-9]_[0-9].nii.gz",
-  #Does the ID have a tails:
-  proc_id_subs=NULL,
-  #How to separate IDs into groups;
-  group_id_sep=c('Nalt','Plac'),
-  #Now set up the model:
-  model.name="con_framing_basic_redo",
-  #Look at the grid! 
-  model.varinames=c("PxH",         
-                    "PxF",
-                    "PxN",
-                    "UxH",
-                    "UxF",
-                    "UxN"),
-  regtype=".1D",
-  #If to convolve with nuisance regressors with dependlab package:
-  ifnuisa=FALSE,
-  #Single subject FSL template path
-  ssub_fsl_templatepath="/Volumes/bek/neurofeedback/scripts/fsl/templates/fsl_cf2_template_R.fsf",
-  adaptive_gfeat=TRUE,
-  #Group level FSL template path
-  gsub_fsl_templatepath="/Volumes/bek/neurofeedback/scripts/fsl/templates/fsl_gfeat_general_adaptive_template.fsf",
-  #Single Subject output root path (before model name folder)
-  ssub_outputroot="/Volumes/bek/neurofeedback/sonrisa2/con_framing/ssanalysis/fsl",
-  #Group lvl output rootpath (before model name folder)
-  glvl_outputroot="/Volumes/bek/neurofeedback/sonrisa2/con_framing/grpanal/fsl",
-  #Brain template path
-  templatedir="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
-  #If there's anyother folder within $output/$model.name that contains *.feat, please remove it from here
-  hig_lvl_path_filter=NULL,
-  whichttest = c("paired","onesample"),
-  cluster_thresh = 3,
-  graphic.threshold=0.95,
-  forcereg=FALSE,
-  ifoverwrite_secondlvl=T
-  #Add more universal arguements in here: 
+#Setting default options
+argu<-as.environment(list(nprocess=4,onlyrun=NULL,proc_id_subs=NULL,regtype=".1D",ifnuisa=FALSE,adaptive_gfeat=TRUE,
+                          hig_lvl_path_filter=NULL,cluster_thresh = 3,whichttest = c("paired","onesample"),
+                          group_id_sep=c('Nalt','Plac'),graphic.threshold=0.95,forcereg=FALSE,ifoverwrite_secondlvl=F,
+                          cfgpath="/Volumes/bek/autopreprocessing_pipeline/Neurofeedback/con_framing.cfg",
+                          regpath="/Volumes/bek/neurofeedback/sonrisa2/con_framing/regs/R_fsl_reg",func.nii.name="nfswudktm*[0-9]_[0-9].nii.gz",
+                          gsub_fsl_templatepath="/Volumes/bek/neurofeedback/scripts/fsl/templates/fsl_gfeat_general_adaptive_template.fsf",
+                          ssub_outputroot="/Volumes/bek/neurofeedback/sonrisa2/con_framing/ssanalysis/fsl",
+                          glvl_outputroot="/Volumes/bek/neurofeedback/sonrisa2/con_framing/grpanal/fsl",
+                          templatedir="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii"
 ))
+
+#Which model to run:
+M_base=FALSE
+M_RTConv=TRUE
+
+#Differentiate argument for different models here:
+if (M_base) {
+  argu$gridpath<-"grid_sc.csv"
+  argu$model.name="con_framing_basic_redo"
+  argu$model.varinames=c("PxH",        
+                         "PxF",
+                         "PxN",
+                         "UxH",
+                         "UxF",
+                         "UxN")
+  argu$ssub_fsl_templatepath="/Volumes/bek/neurofeedback/scripts/fsl/templates/fsl_cf2_template_R.fsf"
+}
+if (M_RTConv) {
+  argu$gridpath<-"grid_sc_rt.csv"
+  argu$model.name="con_framing_rt_conv"
+  argu$model.varinames=c("PxH",        
+                         "PxF",
+                         "PxN",
+                         "UxH",
+                         "UxF",
+                         "UxN")
+  argu$ssub_fsl_templatepath="/Volumes/bek/neurofeedback/scripts/fsl/templates/fsl_cf2_template_R.fsf"
+}
 
 #Get behavioral data
 datalist<-proc_behav_cf(boxdir = boxdir,fmriproc = T)
@@ -76,13 +63,3 @@ fsl_pipe(argu=argu,
          prep.call.func="prep.confram", #This should be a character string that's the name of the prep proc function
          prep.call.allsub=datalist #List of ID list of arguments for prep.call.
 )
-
-
-
-
-
-
-
-
-
-
